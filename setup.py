03b7f3c8ed4ec5baf2855ad3ec0f8b5e504158f4
Simakis Panagiotis <simakis@autistici.org>
2018-03-11 17:22:42 +0200

    update init, setup and manifest

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
+from os import path
 from distutils.core import setup
 
-with open('README.rst') as f:
+here_path = path.abspath(path.dirname(__file__))
+
+
+with open(path.join(here_path, 'README.rst')) as f:
   long_description = f.read()
 
 setup(
   name = 'pyclimenu',
   packages = ['pyclimenu'],
-  version = '0.1.3',
+  version = '0.1.3.3',
   description = 'The easy way to create command line menus',
   author = 'Panagiotis Simakis',
   author_email = 'simakis@autistici.org',
