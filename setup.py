aed9cfa2b6a7e4cecc671aac771084e8dbdd917b
Simakis Panagiotis <simakis@autistici.org>
2018-03-12 22:21:35 +0200

    finilize first working release

--- setup.py
+++ setup.py
@@ -1,22 +1,20 @@
-from os import path
 from distutils.core import setup
 
-here_path = path.abspath(path.dirname(__file__))
-
-
-with open(path.join(here_path, 'README.rst')) as f:
-    long_description = f.read()
-
+def readme():
+    with open('README.rst') as f:
+        return f.read()
 setup(
   name = 'pyclimenu',
   packages = ['pyclimenu'],
-  version = '0.1.1.1',
+  version = '0.1.1.7',
   description = 'The easy way to create command line menus',
   author = 'Panagiotis Simakis',
   author_email = 'simakis@autistici.org',
   url = 'https://github.com/sp1thas/pyclimenu',
   download_url = 'https://github.com/sp1thas/pyclimenu/archive/master.zip',
-  keywords = ['menu', 'command-line'],
+  keywords = ['menu'],
+  include_package_data=True,
   classifiers = [],
-  long_description=long_description,
+  zip_safe=False,
+  long_description=readme()
 )
