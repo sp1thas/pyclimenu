6a8dff91b96850364f951513ca7e881c192dcacc
Simakis Panagiotis <simakis@autistici.org>
2018-03-18 10:44:38 +0200

    add new version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 setup(
   name = 'pyclimenu',
   packages = ['pyclimenu'],
-  version = '0.1.1.9',
+  version = '0.1.2',
   description = 'The easy way to create command line menus',
   author = 'Panagiotis Simakis',
   author_email = 'simakis@autistici.org',
