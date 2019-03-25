b05af6bb47e8b54100fc8806570ceb971e1a76fe
Panagiotis Simakis <sp1thas@autistici.org>
2019-03-25 22:30:54 +0200

    update Readme file and setup script

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
 from distutils.core import setup
+from pyclimenu import __version__
 
-def readme():
-    with open('README.rst') as f:
-        return f.read()
+
+readme_file = open('README.md', 'r')
+long_description = readme_file.read()
+readme_file.close()
+
 setup(
   name = 'pyclimenu',
   packages = ['pyclimenu'],
-  version = '0.1.5',
+  version = __version__,
   description = 'The easy way to create command line menus',
   author = 'Panagiotis Simakis',
   author_email = 'simakis@autistici.org',
   url = 'https://github.com/sp1thas/pyclimenu',
   download_url = 'https://github.com/sp1thas/pyclimenu/archive/master.zip',
   keywords = ['menu'],
-  include_package_data=True,
   classifiers = [],
-  zip_safe=False,
-  long_description=readme()
+  long_description=long_description,
+  long_description_content_type='text/markdown'
 )
